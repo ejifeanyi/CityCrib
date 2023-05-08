c4801053de685dd7885323e30f747b52e94c7a7f
pheany <114763753+pheany@users.noreply.github.com>
2023-05-08 06:04:07 +0100

    CityCrib dev -v1

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import { Nunito } from 'next/font/google';
+
+import './globals.css';
+import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
+
+export const metadata = {
+  title: 'Homely',
+  description: 'Homely app',
+};
+
+const font = Nunito({
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="{font-className">
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
