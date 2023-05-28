c4512c8372a55e663e91b24e6d9a4891c10356b7
pheany <114763753+pheany@users.noreply.github.com>
2023-05-28 19:08:37 +0100

    updated

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { execSync } = require('child_process');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -9,6 +11,13 @@ const nextConfig = {
       'avatars.githubusercontent.com',
       'lh3.googleusercontent.com',
     ],
+  },
+  // Run prisma generate during the production build
+  webpack: (config, { isServer }) => {
+    if (process.env.NODE_ENV === 'production' && isServer) {
+      execSync('npx prisma generate');
+    }
+    return config;
   },
 };
 
